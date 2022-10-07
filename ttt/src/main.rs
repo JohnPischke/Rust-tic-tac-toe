03e4882824b262dfc1b1fdfabd170ebec6806c5f
JohnPischke <100436081+JohnPischke@users.noreply.github.com>
2022-10-07 14:45:31 -0600

    Update main.rs

--- ttt/src/main.rs
+++ ttt/src/main.rs
@@ -2,6 +2,7 @@
 
 use std::io;
 
+// This is our main function. Here we ask if they want to play Tic-Tac-Toe and start it if they say yes
 fn main() {
 
     println!("Do you want to play Two-Player Tic-Tac-Toe?");
@@ -15,13 +16,16 @@ fn main() {
             .expect("Failed to read line");
         
         if response == "yes\r\n"{
-            play_game();
+            play_game(); // The function that runs the game
         }
 
-}
+} 
+
+// This function runs the game.
 
 fn play_game() {
 
+// We create our mutible location variable.
 
     let mut a1 = "-";
     let mut a2 = "-";
@@ -32,13 +36,16 @@ fn play_game() {
     let mut c1 = "-";
     let mut c2 = "-";
     let mut c3 = "-";
-   // let mut win: bool = false;
+
+// We start our gameplay loop here.
+
     println!("Player 1 is x");
     println!("Player 2 is o");
     while true{
 
     print_board(a1,a2,a3,b1,b2,b3,c1,c2,c3);
 
+// Here we get input from Player 1 and record it into the right location.
 
     println!("Player 1 pick your spot");
 
@@ -79,15 +86,18 @@ fn play_game() {
         }
         print_board(a1,a2,a3,b1,b2,b3,c1,c2,c3);
 
+// Here we check if Player 1 is the winner.        
+
         if (a1 == "x") & (a2 == "x") & (a3 == "x") | (b1 == "x") & (b2 == "x") & (b3 == "x") 
         | (c1 == "x") & (c2 == "x") & (c3 == "x") | (a1 == "x") & (b1 == "x") & (c1 == "x") 
         | (a2 == "x") & (b2 == "x") & (c2 == "x") | (a3 == "x") & (b3 == "x") & (c3 == "x") 
         | (a1 == "x") & (b2 == "x") & (c3 == "x") | (a3 == "x") & (b2 == "x") & (c1 == "x"){
-           // win = true;
+
             println!("Player 1 is the winner");
             break;
         }
 
+//Here we get input from player 2 and record it in the right location.
         println!("Player 2 pick your spot");
 
         
@@ -127,26 +137,25 @@ fn play_game() {
         }
         print_board(a1,a2,a3,b1,b2,b3,c1,c2,c3);
 
+// This checks if player 2 is the winner.
         if (a1 == "o") & (a2 == "o") & (a3 == "o") | (b1 == "o") & (b2 == "o") & (b3 == "o") 
         | (c1 == "o") & (c2 == "o") & (c3 == "o") | (a1 == "o") & (b1 == "o") & (c1 == "o") 
         | (a2 == "o") & (b2 == "o") & (c2 == "o") | (a3 == "o") & (b3 == "o") & (c3 == "o") 
         | (a1 == "o") & (b2 == "o") & (c3 == "o") | (a3 == "o") & (b2 == "o") & (c1 == "o"){
-           // win = true;
+
             println!("Player 2 is the winner");
             break;
         }
 
+// This is checking for a draw.
         if (a1 != "-") & (a2 != "-") & (a3 != "-") & (b1 != "-") & (b2 != "-") & (b3 != "-") & (c1 != "-") & (c2 != "-") & (c3 != "-"){
-           // win = true;
             println!("Draw");
             break;
         }
-
-
-
         }
      }
 
+// This function draws the board to the terminal
 
 fn print_board(a1: &str, a2: &str, a3: &str, b1: &str, b2: &str, b3: &str, c1: &str, c2: &str, c3: &str,) {
     println!(" |a|b|c");
